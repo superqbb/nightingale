c5a9eee90b36ed9de485e58ff4e01bf93b9541d3
superqbb <qbb_520@qq.com>
2020-04-18 18:15:03 +0800

    update

--- src/modules/collector/sys/funcs/push.go
+++ src/modules/collector/sys/funcs/push.go
@@ -17,6 +17,7 @@ import (
 )
 
 func Push(items []*dataobj.MetricValue) {
+	//codec是一个序列化工具包，当前使用了msgpack格式进行编码序列化，MessagePack是一种有效的二进制序列化格式。就像JSON。但又快又小。
 	var mh codec.MsgpackHandle
 	mh.MapType = reflect.TypeOf(map[string]interface{}(nil))
 
@@ -24,6 +25,7 @@ func Push(items []*dataobj.MetricValue) {
 	count := len(addrs)
 	retry := 0
 	for {
+		//rand.Perm 将count个transfer随机均匀打散到数组数组下标
 		for _, i := range rand.Perm(count) {
 			addr := addrs[i]
 			conn, err := net.DialTimeout("tcp", addr, time.Millisecond*3000)
@@ -38,6 +40,7 @@ func Push(items []*dataobj.MetricValue) {
 				*bufio.Writer
 			}{conn, bufio.NewReader(conn), bufio.NewWriter(conn)}
 
+			//【不同点】：open-falcon的RPC，使用的是json格式进行传输（net.JsonRpcClient），而夜莺使用的是msgpack，网络传输数据包更小，速度更快，解析速度也比json快。
 			rpcCodec := codec.MsgpackSpecRpc.ClientCodec(bufconn, &mh)
 			client := rpc.NewClientWithCodec(rpcCodec)
 
